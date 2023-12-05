188c0ec92c63d4473f72f2c4761e61f736918d7e
theomantz <theo@mantz.nyc>
2023-12-04 23:13:29 -0500

    work on day 3

--- 2023/day-03/problem-01.py
+++ 2023/day-03/problem-01.py
@@ -1,6 +1,17 @@
+import sys
 import re
+from pprint import pprint
+class Node:
+    def __init__(self, m: re.Match, row):
+        self.value = m.group(0)
+        self.start_index = m.start()
+        self.end_index = m.end()
+        self.row_index = row
+        
+    def __str__(self):
+        return str("v: {self.value}, si: {self.start_index}, ei: {self.end_index}, ri: {self.row_index}".format(self=self))
 class Grid:
-    grid_row_regex = re.compile(r'\d+|.+|[*#+-@#]')
+    grid_row_regex = re.compile(r'(\d+)|(\.+)|([*#+-@#])')
     def __init__(self, grid_string):
         self.grid = []
         self.build_grid(grid_string)
@@ -10,31 +21,33 @@ class Grid:
             self.grid.append(self.build_row(row, index))
 
     def build_row(self, row_string, column_index):
-        return self.grid_row_regex.sub(row_string, lambda m: self.build_node(m, column_index))
+        return [self.build_node(i, column_index) for i in self.grid_row_regex.finditer(row_string) if i is not None and i != '']
 
-    def build_node(self, m: re.Match, column_index):
+    def append_row(self, row_string):
+        self.grid.append(self.build_row(row_string, len(self.grid) - 1))
+
+    def build_node(self, m: re.Match, column_index) -> Node:
         if m.group(0).isdigit():
             return Number(m, column_index)
         else:
             return Symbol(m, column_index)
+        
+    def __str__(self):
+        return str([[str(node) for node in row] for row in self.grid])
 
-class Number:
+
+
+class Number(Node):
     def __init__(self, m: re.Match, row):
+        self.value = m.group(0)
         self.number = m.group(0)
         self.start_index = m.start()
         self.end_index = m.end()
         self.row_index = row
 
-    def __str__(self):
-        return { 
-            number: str(self.number),
-            start_index: str(self.start_index),
-            end_index: str(self.end_index),
-            row_index: str(self.row_index)
-        }
-
-class Symbol:
+class Symbol(Node):
     def __init__(self, m: re.Match, row):
+        self.value = m.group(0)
         self.symbol = m.group(0)
         self.start_index = m.start()
         self.end_index = m.end()
@@ -47,10 +60,11 @@ class Symbol:
     def build_row(self, row_string):
         return [char for char in row_string]
 
-    def __str__(self):
-        return {
-            symbol: str(self.symbol),
-            start_index: str(self.start_index),
-            end_index: str(self.end_index),
-            row_index: str(self.row_index)
-        }
+class Solution:
+    def solve(self, file):
+        for grid in open(file):
+            grid = Grid(grid)
+            print(grid)
+            
+
+Solution().solve(sys.argv[1])
